eb927f54f4b76e5c9469923108ebb291e2e91620
Butterbombe <butterbombe7@gmail.com>
2023-10-05 03:24:50 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+let tasks = [];
+
+function addToList(){
+    const input = document.getElementById("input");
+    const parentElement = document.getElementById("list-body");
+
+    if(input.value === "")
+    { 
+        return;
+    }
+    //create html element
+    const paragraph = document.createElement("p");
+    //add + hide css
+    paragraph.classList.add("task");
+    paragraph.classList.toggle("task");
+    //add event
+    paragraph.addEventListener("click", markAsDone);
+    //assign input text
+    paragraph.innerHTML = input.value + ".";
+    input.value = "";
+    //display element
+    parentElement.appendChild(paragraph);
+}
+//add enter <------ to add
+
+function clearList()
+{
+    const parentElement = document.getElementById("list-body");
+
+    while (parentElement.firstChild) {
+        parentElement.removeChild(parentElement.firstChild);
+      }
+}
+
+function markAsDone(){
+    this.classList.toggle("task");
+}
